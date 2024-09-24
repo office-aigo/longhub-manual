fdafe68195063b9902e658a07505f327f6b8d34a
office-aigo <134594258+office-aigo@users.noreply.github.com>
2024-09-24 09:33:50 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  base: "/starthere/", //目标地址是：https://openhacking.github.io/vuepress-template/，所以需要配置base地址后缀
+  base: "/manual/", //目标地址是：https://openhacking.github.io/vuepress-template/，所以需要配置base地址后缀
   locales: {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
     "/": {
       lang: "zh-CN", // 将会被设置为 <html> 的 lang 属性
-      title: "Start Here",
-      description: "Start Here",
+      title: "longhub | manual",
+      description: "Manual",
     },
   },
   plugins: [
